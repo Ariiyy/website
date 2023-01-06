c8b8802d0972617f73c3e41eb5e1b54037746e3b
Ari <aricrafts34@gmail.com>
2023-01-06 09:20:30 -0500

    webvilletunes

---
+++ webvlletunes/playlist.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("addButton");
+function handleButtonClick() {alert("Button was clicked!");}
+var button = document.getElementById("addButton"); 
+button.onclick = handleButtonClick;
+window.onload = init;
+function init() {var button = document.getElementById("addButton");}
+function handleButtonClick() {alert("Button was clicked!");}
