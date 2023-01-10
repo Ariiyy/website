03fed76f353c65078ad239403b66a1df0bcba103
Ari <aricrafts34@gmail.com>
2023-01-10 09:25:01 -0500

    button

--- webvlletunes/playlist.js
+++ webvlletunes/playlist.js
@@ -1,21 +1,26 @@
+window.onload = initi;
+
 var button = document.getElementById("addButton");
 function handleButtonClick() {alert("Button was clicked!");}
 var button = document.getElementById("addButton"); 
 button.onclick = handleButtonClick;
-window.onload = init;
-function init() {var button = document.getElementById("addButton");}
+
+function initi() {var button = document.getElementById("addButton");
+button.onclick = handleButtonClick;
+loadPlaylist();
+}
 function handleButtonClick() {alert("Button was clicked!");}
 li.innerHTML = songName;
 function handleButtonClick() {
-    var textInput = document.getElementById("songTextInput");
-    var songName = textInput.value;
-    alert("adding " + songName);
-}    
+var textInput = document.getElementById("songTextInput");
+var songName = textInput.value;
+alert("adding " + songName);}    
 function handleButtonClick() {
-    var textInput = document.getElementById("songTextInput");
-    var songName = textInput.value;
-    var li = document.createElement("li"); 
-    li.innerHTML = songName;
-    var ul = document.getElementById("playlist"); 
-    ul.appendChild(li);
+var textInput = document.getElementById("songTextInput");
+var songName = textInput.value;
+var li = document.createElement("li"); 
+li.innerHTML = songName;
+var ul = document.getElementById("playlist"); 
+ul.appendChild(li);
+save(songName);
 }
