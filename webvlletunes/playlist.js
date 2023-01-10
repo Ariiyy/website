7a189f251326aa6f7d8cba775c1686a3405762d4
Ari <aricrafts34@gmail.com>
2023-01-10 08:55:29 -0500

    songlist update

--- webvlletunes/playlist.js
+++ webvlletunes/playlist.js
@@ -5,3 +5,17 @@ button.onclick = handleButtonClick;
 window.onload = init;
 function init() {var button = document.getElementById("addButton");}
 function handleButtonClick() {alert("Button was clicked!");}
+li.innerHTML = songName;
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;
+    alert("adding " + songName);
+}    
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;
+    var li = document.createElement("li"); 
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist"); 
+    ul.appendChild(li);
+}
